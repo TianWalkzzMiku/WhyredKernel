c6d9c4a30f96fdd947904222294fd9ecd9031387
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-24 22:11:48 -0800

    msm: mdss: Reduce latency while processing atomic ioctls
    Unfortunately, our display driver must occasionally sleep inside
    start_atomic(), which causes the CPU running the ioctl to schedule and
    potentially idle. Depending on how deeply the running CPU idles, there
    can be quite a bit of latency added to processing the "atomic" ioctl as
    a result, which hurts display rendering latency. We can alleviate this
    by optimistically assuming that the task running the ioctl won't
    migrate CPUs while it runs and restricting the current CPU to shallow
    idle states.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Ratoriku: Port to mdss]
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -39,6 +39,7 @@
 #include <linux/kthread.h>
 #include <linux/sched.h>
 #include <uapi/linux/sched/types.h>
+#include <linux/pm_qos.h>
 #ifdef CONFIG_MACH_MI
 #include <linux/interrupt.h>
 #include <linux/pm_wakeup.h>
@@ -4859,7 +4860,7 @@ err:
 	return ret;
 }
 
-static int mdss_fb_atomic_commit_ioctl(struct fb_info *info,
+static int __mdss_fb_atomic_commit_ioctl(struct fb_info *info,
 	unsigned long *argp, struct file *file)
 {
 	int ret, i = 0, j = 0, rc;
@@ -5021,6 +5022,28 @@ err:
 		kfree(ds_data);
 	}
 
+	return ret;
+}
+
+int mdss_fb_atomic_commit_ioctl(struct fb_info *info,
+	unsigned long *argp, struct file *file)
+{
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to finish running the ioctl whenever the ioctl runs a
+	 * command that sleeps, such as for an "atomic" commit.
+	 */
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
+	};
+	int ret;
+ 
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
+	ret = __mdss_fb_atomic_commit_ioctl(info, argp, file);
+	pm_qos_remove_request(&req);
+ 
 	return ret;
 }
 
