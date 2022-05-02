dab20e68a1cb3efa3d3d8332fb82f8cec8df086d
Minchan Kim <minchan@google.com>
2022-05-02 12:44:15 -0700

    ANDROID: fix ABI breakage caused by per_cpu_pages
    The patchset adds a new spin_lock field into per_cpu_pages which
    breaks KMI so this patch introduces per_cpu_pages_ext and
    per_cpu_pageset_ext and changes relavant functions and code
    to use the _ext data structures instead of original one.
    
    Bug: 230899966
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: Ic5156f784223695c9716409036e2973df69ef99b

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -505,7 +505,6 @@ enum zone_watermarks {
 #define high_wmark_pages(z) (z->watermark[WMARK_HIGH])
 
 struct per_cpu_pages {
-	spinlock_t lock;	/* Protects lists field */
 	int count;		/* number of pages in the list */
 	int high;		/* high watermark, emptying needed */
 	int batch;		/* chunk size for buddy add/remove */
@@ -524,6 +523,11 @@ struct per_cpu_pageset {
 	s8 stat_threshold;
 	s8 vm_stat_diff[NR_VM_ZONE_STAT_ITEMS];
 #endif
+};
+
+struct per_cpu_pageset_ext {
+	spinlock_t lock;	/* Protects pageset.pcp.lists field */
+	struct per_cpu_pageset pageset;
 };
 
 struct per_cpu_nodestat {
